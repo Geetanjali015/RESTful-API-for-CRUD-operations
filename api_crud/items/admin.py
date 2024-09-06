fe551183bcbe07594b177cfe0c3be689a033ec6b
Geetanjali Reddy <geetareddy2005@gmail.com>
2024-09-06 19:12:12 +0530

    first commit

---
+++ api_crud/items/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Member
+# Register your models here.
+class MemberAdmin(admin.ModelAdmin):
+    list_display="firstname","lastname","country"
+
+admin.site.register(Member,MemberAdmin)
---
+++ api_crud/items/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Member(models.Model):
+    firstname=models.CharField(max_length=100)
+    lastname=models.CharField(max_length=100)
+    country=models.CharField(max_length=100)
